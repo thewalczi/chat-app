8b4d464a954464604ced84c114a2c1f8ce835378
Kamil Walczak <kamilwalczak@MacBook-Air-Kamil.local>
2025-12-30 20:56:10 +0100

    Add prettier

--- server.js
+++ server.js
@@ -1,46 +1,49 @@
-const http = require("http")
-const {WebSocketServer} = require ("ws")
-const fs = require("fs")
-const path = require("path")
-const mime = require("mime-types")
+const http = require('http')
+const { WebSocketServer } = require('ws')
+const fs = require('fs')
+const path = require('path')
+const mime = require('mime-types')
 
 const server = http.createServer((req, res) => {
-  let filePath = path.join(
-    __dirname,
-    "dist",
-    req.url === "/" ? "index.html" : req.url
-  )
+    let filePath = path.join(
+        __dirname,
+        'dist',
+        req.url === '/' ? 'index.html' : req.url
+    )
 
-  const contentType = mime.lookup(filePath) || "application/octet-stream"
+    const contentType = mime.lookup(filePath) || 'application/octet-stream'
 
-  fs.readFile(filePath, (err, content) => {
-    if (err) {
-      fs.readFile(path.join(__dirname, "dist", "index.html"), (err, html) => {
-        res.writeHead(200, { "Content-Type": "text/html" })
-        res.end(html)
-      })
-    } else {
-      res.writeHead(200, { "Content-Type": contentType })
-      res.end(content)
-    }
-  })
+    fs.readFile(filePath, (err, content) => {
+        if (err) {
+            fs.readFile(
+                path.join(__dirname, 'dist', 'index.html'),
+                (err, html) => {
+                    res.writeHead(200, { 'Content-Type': 'text/html' })
+                    res.end(html)
+                }
+            )
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType })
+            res.end(content)
+        }
+    })
 })
 
-const wss = new WebSocketServer({server})
+const wss = new WebSocketServer({ server })
 
-wss.on("connection", (ws) => {
-    console.log("Client connected to instance:", process.pid)
+wss.on('connection', (ws) => {
+    console.log('Client connected to instance:', process.pid)
 
-    ws.on("message", (data) => {
+    ws.on('message', (data) => {
         let message
 
         try {
-          message = JSON.parse(data)
+            message = JSON.parse(data)
         } catch {
-          alert('Connection is lost.')
+            alert('Connection is lost.')
         }
 
-        if(message.type === "message") {
+        if (message.type === 'message') {
             message.timestamp = Date.now()
         }
 
@@ -51,8 +54,8 @@ wss.on("connection", (ws) => {
         })
     })
 
-    ws.on("close", () => {
-        console.log("Client disconnected")
+    ws.on('close', () => {
+        console.log('Client disconnected')
     })
 })
 
