9cd2021d98c3101288152d159ec3d39bb9c3ef6e
Kamil Walczak <kamilwalczak@MacBook-Air-Kamil.local>
2025-12-30 20:37:05 +0100

    Move logic outise App file

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,91 +1,11 @@
-import { useEffect, useRef, useState } from 'react';
-import './App.css'
+import ChatView from './components/chat-view/ChatView';
 
-interface Message {
-  type: string,
-  timestamp: number,
-  username: string | null,
-  text: string
-} 
-
-const WS_URL = import.meta.env.PROD
-    ? "wss://chat-app-l6fi.onrender.com"
-    : "ws://localhost:3000"
-
-
-function App() {
-  const [message, setMessage] = useState<string>('')
-  const [messages, setMessages] = useState<Message[]>([])
-  const [user, setUser] = useState<string | null>(null)
-  const [socket, setSocket] = useState<WebSocket | null>(null)
-  
-  const reconnectTimeoutRef = useRef<number | null>(null)
-  
-  const getDateTime = (timestamp: number) => new Date(timestamp).toLocaleString('pl-PL')
-  
-  const handleSendMessage = () => {
-    const newMessage: Omit<Message, 'timestamp'> = {
-      type: "message",
-      username: user,
-      text: message
-    }
-    
-    socket?.send(JSON.stringify(newMessage))
-    setMessage('')
-  }
-  
-  useEffect(() => {
-    let username = prompt('Write you name') || ''
-    setUser(username)
-  }, [])
-
-  useEffect(() => {
-    if (!user) return
-
-    const connect = () => {
-      const ws = new WebSocket(WS_URL)
-      setSocket(ws)
-
-      ws.onopen = () => {
-        console.log("Connected to websocket server")
-      }
-
-      ws.onmessage = (msg) => {
-        console.log(msg)
-        setMessages((prev) => [...prev, JSON.parse(msg.data)])
-      }
-
-      ws.onclose = () => {
-        console.log("Connection lost, retrying...")
-        reconnectTimeoutRef.current = window.setTimeout(connect, 2000)
-      }
-      ws.onerror = () => {
-        WS_URL.toLocaleLowerCase()
-      }
-    }
-
-    connect()
-
-  }, [user])
-  
-
-
+const App = () => {
   return (
-    <>
-    <h2>{user}</h2>
-      <input value={message} onChange={(e) => setMessage(e.target.value)}/>
-      <button onClick={handleSendMessage}>Send message</button>
-      <ul>
-        {messages.map((msg, i) => (
-          <li key={`${i}_${msg.text}`}>
-            <div><i>{getDateTime(msg.timestamp)}</i></div>
-            <h4>{msg.username}</h4>
-            <span>{msg.text}</span>
-          </li>
-        ))}
-      </ul>
-    </>
-  )
-}
+      <>
+        <ChatView/>
+      </>
+    )
+  }
 
 export default App
